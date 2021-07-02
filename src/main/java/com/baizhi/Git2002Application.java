49d35242a2b9658d7414bef270d8268018d6709c
czz <18225372273@163.com>
2021-07-03 00:50:29 +0800

    初始化环境

---
+++ src/main/java/com/baizhi/Git2002Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2002Application.class, args);
+    }
+
+}
