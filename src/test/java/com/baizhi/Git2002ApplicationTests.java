338d0c8031a215be44f66b1657ffec0c5150260c
czz <18225372273@163.com>
2021-07-03 00:52:41 +0800

    初å§测试环境一境â

--- src/test/java/com/baizhi/Git2002ApplicationTests.java
+++ src/test/java/com/baizhi/Git2002ApplicationTests.java
@@ -8,6 +8,14 @@ class Git2002ApplicationTests {
 
     @Test
     void contextLoads() {
+        int a=100;
+        int b=200;
+        System.out.println("sjahdsf");
     }
-
+    @Test
+    void contextLoadss() {
+        int a=10*5;
+        int b=200-20;
+        System.out.println("s士大夫sf");
+    }
 }
